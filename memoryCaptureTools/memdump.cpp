f8ad759b3799fa317d56d6fc21df1fa26cd2b3c0
tinoryj <tinoryj@gmail.com>
2020-10-27 10:33:59 +0800

    add new memory capture tools with ptrace method

---
+++ memoryCaptureTools/memdump.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <sys/ptrace.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+void dump_memory_region(FILE* pMemFile, unsigned long start_address, long length, int serverSocket)
+{
+    unsigned long address;
+    int pageLength = 4096;
+    unsigned char page[pageLength];
+    fseeko(pMemFile, start_address, SEEK_SET);
+
+    for (address=start_address; address < start_address + length; address += pageLength)
+    {
+        fread(&page, 1, pageLength, pMemFile);
+        if (serverSocket == -1)
+        {
+            // write to stdout
+            fwrite(&page, 1, pageLength, stdout);
+        }
+        else
+        {
+            send(serverSocket, &page, pageLength, 0);
+        }
+    }
+}
+
+int main(int argc, char **argv) {
+
+    if (argc == 2 || argc == 4)
+    {
+        int pid = atoi(argv[1]);
+        long ptraceResult = ptrace(PTRACE_ATTACH, pid, NULL, NULL);
+        if (ptraceResult < 0)
+        {
+            printf("Unable to attach to the pid specified\n");
+            return 0;
+        }
+
+        char mapsFilename[1024];
+        sprintf(mapsFilename, "/proc/%s/maps", argv[1]);
+        FILE* pMapsFile = fopen(mapsFilename, "r");
+        char memFilename[1024];
+        sprintf(memFilename, "/proc/%s/mem", argv[1]);
+        FILE* pMemFile = fopen(memFilename, "r");
+        char line[256];
+        while (fgets(line, 256, pMapsFile) != NULL)
+        {
+            unsigned long start_address;
+            unsigned long end_address;
+            sscanf(line, "%08lx-%08lx\n", &start_address, &end_address);
+            dump_memory_region(pMemFile, start_address, end_address - start_address, -1);
+        }
+        fclose(pMapsFile);
+        fclose(pMemFile);
+
+        ptrace(PTRACE_CONT, pid, NULL, NULL);
+        ptrace(PTRACE_DETACH, pid, NULL, NULL);
+    }
+    else
+    {
+        printf("%s <pid>\n", argv[0]);
+        printf("%s <pid> <ip-address> <port>\n", argv[0]);
+        exit(0);
+    }
+}
